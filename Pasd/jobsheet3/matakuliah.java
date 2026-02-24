09da674a5304b5f0a12d6b7cfb34ccbedbded772
hamzyhams <nexthamas95@gmail.com>
2026-02-24 19:55:18 +0700

    Add files via upload

---
+++ Pasd/jobsheet3/matakuliah.java
@@ -0,0 +1,15 @@
+package pasd3;
+
+public class matakuliah {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jam;
+
+    public matakuliah(String kode, String nama, int sks, int jam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jam = jam;
+    }
+}
